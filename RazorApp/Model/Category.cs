076453c0187529fa7d7cce6a610f070e76f91acc
unknown <ahmedwoye@gmail.com>
2022-07-02 09:24:22 +0100

    updated records

---
+++ RazorApp/Model/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RazorApp.Model
+{
+    public class Category
+    {
+        [Key]
+        public int MyProperty { get; set; }
+
+        [Required]
+        public int Name { get; set; }
+
+        public int DisplayOrder { get; set; }
+    }
+}
